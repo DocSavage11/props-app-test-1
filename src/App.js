8de1aad56b7a4f11dfa0ed0a53ddc80732d1cafb
Doc Savage <97268006+DocSavage11@users.noreply.github.com>
2022-06-26 14:37:34 -0500

    Basic props app complete.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import Contact from './Components/Contact'
+
+// Contact Profile Imagies Start
 import team1 from "./Assests/Images/team1.jpg"
 import team2 from "./Assests/Images/team2.jpg"
 import team3 from "./Assests/Images/team3.jpg"
 import team4 from "./Assests/Images/team4.jpg"
+// Contact Profile Imagies End
 
+// Social Bar Icons Start 
+import InstagramIcon from './Assests/Icons/InstagramIcon.png'
+import LinkedinIcon from './Assests/Icons/LinkedinIcon.png'
+import TwitterIcon from './Assests/Icons/TwitterIcon.png'
+// Social Bar Icons End
 function App() {
   return (
     <div className="contacts">
@@ -13,25 +21,46 @@ function App() {
           name="Lockheed Martin"
           phone="1 (309) 555-1234"
           email="lockheedmartin@gmail.meow"
+          about="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore cum nulla placeat quos necessitatibus facere quaerat, id exercitationem alias accusantium error similique fugiat unde architecto deleniti ab iste mollitia tempore."
+
+          InstagramIcon={InstagramIcon}
+          LinkedinIcon={LinkedinIcon}
+          TwitterIcon={TwitterIcon}
+
       />
-      {/* <Contact 
-          img="./images/fluffykins.png"
+      <Contact 
+          image={team2}
           name="Vivian Taylor"
           phone="1 (212) 555-2345"
           email="viviantaylor@gmail.com"
+          about="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore cum nulla placeat quos necessitatibus facere quaerat, id exercitationem alias accusantium error similique fugiat unde architecto deleniti ab iste mollitia tempore."
+          
+          InstagramIcon={InstagramIcon}
+          LinkedinIcon={LinkedinIcon}
+          TwitterIcon={TwitterIcon}
       />
       <Contact 
-          img="./images/felix.png"
+          image={team3}
           name="Daniel Rook"
           phone="1 (212) 555-4567"
           email="danielrook@gmail.com"
+          about="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore cum nulla placeat quos necessitatibus facere quaerat, id exercitationem alias accusantium error similique fugiat unde architecto deleniti ab iste mollitia tempore."
+          
+          InstagramIcon={InstagramIcon}
+          LinkedinIcon={LinkedinIcon}
+          TwitterIcon={TwitterIcon}
       />
       <Contact 
-          img="./images/pumpkin.png"
-          name="John Lark"
+          image={team4}
+          name="Montana" 
           phone="1 (455) 221 6590"
           email="johnlark@gmail.com"
-      /> */}
+          about="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore cum nulla placeat quos necessitatibus facere quaerat, id exercitationem alias accusantium error similique fugiat unde architecto deleniti ab iste mollitia tempore."
+          
+          InstagramIcon={InstagramIcon}
+          LinkedinIcon={LinkedinIcon}
+          TwitterIcon={TwitterIcon}
+      />
   </div>
   );
 }
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -7,12 +7,24 @@ export default function Contact(props) {
         <div className="Contact-Card">
             <img id='ProfilePicture' src={props.image} alt="" />
 
-            <p id="ContactName">{props.name}</p>
-
             <div className="Contact-Information">
+                <p id="ContactName">{props.name}</p>
+
                 <p id="PhoneNumber">{props.phone}</p>
 
                 <p id="Email">{props.email}</p>
+            </div>
+
+            <div className="About-Contact">
+                <h3 id='AboutTitle'>About Contact</h3>
+
+                <p id="about">{props.about}</p>
+            </div>
+
+            <div className="Social-Icon-Bar">
+                <img id='SocialIcon' src={props.InstagramIcon} alt="" />
+                <img id='SocialIcon' src={props.LinkedinIcon} alt="" />
+                <img id='SocialIcon' src={props.TwitterIcon} alt="" />
             </div>
         </div>
     )
